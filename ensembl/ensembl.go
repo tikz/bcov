eadb214ea0dd6bbc6e2dab11c13d56a0f04f5127
Mauro Song <maurosong@protonmail.com>
2022-05-06 22:13:10 -0300

    Update query

--- ensembl/ensembl.go
+++ ensembl/ensembl.go
@@ -31,27 +31,27 @@ func GetExons(db *sqlx.DB) ([]Region, error) {
 		seq_region.name AS chrom,
 		exon.seq_region_start AS start,
 		exon.seq_region_end AS end,
-		gene_names.display_label AS gene_hgnc,
+		gene_names.dbprimary_acc AS gene_hgnc,
 		gene_names.display_label AS gene_name,
-		gene_names.display_label AS gene_description,
+		gene_names.description AS gene_description,
 		exon_transcript.rank AS exon_number,
-		gene.stable_id as stable_id
+		gene.stable_id as stable_id,
 		FROM gene
-
+		
 		JOIN transcript ON transcript.transcript_id = gene.canonical_transcript_id
 		JOIN exon_transcript ON exon_transcript.transcript_id = transcript.transcript_id
 		JOIN exon ON exon_transcript.exon_id = exon.exon_id
 		JOIN seq_region ON seq_region.seq_region_id = gene.seq_region_id
-
+		
 		LEFT JOIN (
-			SELECT xref_id, display_label
+			SELECT xref_id, dbprimary_acc, display_label, description
 			FROM xref
 			WHERE xref.external_db_id = 1100
 		) AS gene_names ON gene_names.xref_id = gene.display_xref_id
-
+		
 		WHERE LEFT(gene.stable_id, 4) <> 'LRG_' AND gene_names.display_label IS NOT NULL
 		AND seq_region.name IN ("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y", "MT")
-
+		
 		ORDER BY CAST(seq_region.name as unsigned), exon.seq_region_start;
 	`
 
