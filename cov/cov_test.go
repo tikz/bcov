ccd98d3d3898f8b6bf0e2ca99235f5a5aca14153
Mauro Song <maurosong@protonmail.com>
2022-09-22 14:51:10 -0300

    Add BAM coverage unittest

--- cov/cov_test.go
+++ cov/cov_test.go
@@ -1,1 +1,42 @@
 package cov
+
+import (
+	"testing"
+)
+
+// func TestBamWorker(t *testing.T) {
+
+// 	bamReader, err := bam.NewReader("../tests/little.bam")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	exons := []Exon{{
+// 		Chromosome:    "1",
+// 		Start:         100,
+// 		End:           200,
+// 		ExonNumber:    1,
+// 		PositionDepth: make(map[Position]Depth),
+// 	}}
+// 	fmt.Println("running workesr")
+// 	rChan := make(chan Exon)
+// 	// bamWorker(bamReader, exons, rChan)
+
+// 	// fmt.Println(<-rChan)
+
+// }
+
+func TestExonDepth(t *testing.T) {
+	exon := ExonDepth("../tests/little.bam", "1", 100, 100000)
+	testPos := func(pos int, expectedCount int) {
+		count := exon.PositionDepth[Position(pos)]
+		if count != Depth(expectedCount) {
+			t.Errorf("Expected pos %d read count to be %d, got %d", pos, expectedCount, count)
+		}
+	}
+
+	testPos(70017, 16)
+	testPos(69956, 40)
+	testPos(99270, 1)
+
+}
--- utils/hash_test.go
+++ utils/hash_test.go
@@ -12,8 +12,6 @@ func TestSHA256FileHash(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"test.bed open", args{path: "x"}, "", true},
-		{"test.bed hash", args{path: "../tests/test.bed"}, "6466a939feb2a7f0ef7b88e0dd473b64e2ee71f69382de0a7247ca938d3464de", false},
 		{"little.bam open", args{path: "x"}, "", true},
 		{"little.bam hash", args{path: "../tests/little.bam"}, "238598669fe0d1a510b0ed76577943b6142e6fef29f082536a399ef3225c4ad4", false},
 	}
