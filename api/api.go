e65c0055b72185fbe724776ff3218bf548f1c116
Mauro Song <maurosong@protonmail.com>
2022-10-19 17:41:28 -0300

    Refactor

--- api/api.go
+++ api/api.go
@@ -9,13 +9,48 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 
+	cache "github.com/chenyahui/gin-cache"
+	"github.com/chenyahui/gin-cache/persist"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/static"
 	"github.com/tikz/bcov/db"
 
 	"github.com/gin-gonic/gin"
 )
 
-// JSON API endpoints for the backend
+func Endpoints() *gin.Engine {
+	cacheDuration := 24 * 7 * time.Hour
+	memoryStore := persist.NewMemoryStore(cacheDuration)
+
+	r := gin.Default()
+	r.Use(cors.Default())
+
+	r.Use(static.Serve("/", static.LocalFile("web/build", true)))
+
+	r.GET("/api/kits", KitsEndpoint)
+	r.GET("/api/kit/:id", KitEndpoint)
+	r.GET("/api/gene/:id", GeneEndpoint)
+
+	r.GET("/api/search/genes/:name", cache.CacheByRequestURI(memoryStore, cacheDuration), SearchGenesEndpoint)
+	r.GET("/api/search/kits/:name", cache.CacheByRequestURI(memoryStore, cacheDuration), SearchKitsEndpoint)
+	r.GET("/api/search/variant/:id", cache.CacheByRequestURI(memoryStore, cacheDuration), SearchVariantEndpoint)
+
+	r.GET("/api/reads/:kit_id/:exon_id", cache.CacheByRequestURI(memoryStore, cacheDuration), ReadsEndpoint)
+	r.GET("/api/depth-coverages/:kit_id/:exon_id", cache.CacheByRequestURI(memoryStore, cacheDuration), DepthCoveragesEndpoint)
+	r.GET("/api/variants/:kit_id/:exon_id", cache.CacheByRequestURI(memoryStore, cacheDuration), VariantsEndpoint)
+	r.GET("/api/bams/:kit_id", BAMsEndpoint)
+	r.GET("/api/variants-csv/:gene_name", cache.CacheByRequestURI(memoryStore, cacheDuration), VariantsCSVEndpoint)
+
+	r.NoRoute(func(c *gin.Context) {
+		c.File("web/build/index.html")
+	})
+
+	return r
+}
+
+// Endpoints logic
 
 // KitsEndpoint returns all the kits in the database
 func KitsEndpoint(c *gin.Context) {
